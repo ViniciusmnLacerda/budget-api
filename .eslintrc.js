a9dae3e8b117e2ff7377a60492531553694290e8
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-02 12:03:13 -0300

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  '@typescript-eslint/estrict-boolean-expressions': 'off',
+  '@typescript-eslint/no-misused-promises': 'off',
+  '@typescript-eslint/promise-function-async': 'off',
+  '@typescript-eslint/method-signature-style': 'off',
+  '@typescript-eslint/no-invalid-void-type': 'off',
+  '@typescript-eslint/no-extra-semi': 'off'
+}
