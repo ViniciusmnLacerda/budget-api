1e19d35c0824a2148626b16aae4cde4706ef65d5
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-02 17:31:37 -0300

    fix lint budget model

--- src/Models/Budget.model.ts
+++ src/Models/Budget.model.ts
@@ -4,7 +4,7 @@ import { IProduct, IUser } from '../Interfaces';
 export default class BudgetModel {
   public getBudget = async (selectedProducts: IProduct[], { tax }: IUser): Promise<number> => {
     const budget = selectedProducts
-      .reduce((acc, { price}) => acc + ((price * tax) / 100), 0);
+      .reduce((acc, { price }) => acc + ((price * tax) / 100), 0);
     
     return budget;
   }
