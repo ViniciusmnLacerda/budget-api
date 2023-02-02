cb066845c2c61d18a65a3860f642571153d4c837
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-02 13:02:38 -0300

    feat: GET /user

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import { userRouter } from './Routes';
 
 export default class App {
   public app: express.Express;
 
   constructor() {
     this.app = express();
+
+    this.app.use('/user', userRouter)
   }
 
   public start(PORT: string | number):void {
