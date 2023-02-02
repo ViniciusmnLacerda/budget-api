211821da1132c86750e40bba5bc4fd153780c7e8
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-02 12:37:11 -0300

    feat: server and app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
-  '@typescript-eslint/estrict-boolean-expressions': 'off',
-  '@typescript-eslint/no-misused-promises': 'off',
-  '@typescript-eslint/promise-function-async': 'off',
-  '@typescript-eslint/method-signature-style': 'off',
-  '@typescript-eslint/no-invalid-void-type': 'off',
-  '@typescript-eslint/no-extra-semi': 'off'
+  extends: 'standard-with-typescript',
+  parseOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/estrict-boolean-expressions': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/no-invalid-void-type': 'off',
+    '@typescript-eslint/no-extra-semi': 'off'
+  }
 }
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
